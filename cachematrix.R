feb3b4ca6224bc1f55cd76e34967c5c71b8e566c
Aya2018 <36399998+Aya2018@users.noreply.github.com>
2018-03-12 16:41:53 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = martix()){
+        inv <- NULL
+        set <- function(y){
+                x <<- y     #define the value of "set"
+                inv <<- NULL  #reset the value of "setinvs"
+        }
+        get <- function() x #print the value of "set"
+        setinvs <- function(inverse) inv <<- inverse #define the value of "setinvs"
+        getinvs <- function () inv  #print the value of inv
+        
+        list(set = set, get = get, set_invs = setinvs, get_invs = getinvs)
+}
 
-makeCacheMatrix <- function(x = matrix()) {
-
+cacheSolve <- function(x,...){
+        inv <- x$get_invs()
+        if(!is.null(inv)){
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        x$set_invs(inv)
+        inv
 }
 
 
-## Write a short comment describing this function
+#testing-------------------------------------------------------------
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+testmatrix_1 <- matrix(c(2,1,5,3),2,2)
+det(testmatrix_1)
+
+testmatrix_2 <- matrix( c(5, 1, 0,
+                          3,-1, 2,
+                          4, 0,-1), nrow=3, byrow=TRUE)
+det(testmatrix_2)
+
+
+my_matrix <- makeCacheMatrix(testmatrix_2)
+
+my_matrix$set(testmatrix_1)
+my_matrix$get()
+my_matrix$set_invs()
+my_matrix$get_invs()
+
+cacheSolve(my_matrix)
